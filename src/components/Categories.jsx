aac7b5e0bbe192b8b7d478f53ff1deccba352fae
Diyorbek1213-sys <birorbirorkim@gmail.com>
2025-03-10 19:26:05 +0500

    7 oy 8 uyy

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -6,7 +6,7 @@ const Categories = () => {
   const { data, error, isLoading } = useGetCategoriesQuery()
 
   return (
-    <div className='flex gap-4 overflow-scroll'>
+    <div className='flex gap-4 overflow-scroll mb-7'>
       {
         data && data.length > 0 && data.map((item, index) => {
           return <Link to={`/filter/${item}`} className='p-2 rounded-md cursor-pointer bg-gray-200 text-gray-700 hover:bg-gray-700 hover:text-gray-200' key={index}>{item}</Link>
